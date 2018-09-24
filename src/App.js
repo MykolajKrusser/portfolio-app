76ef6ce0dead14b8eec1a4ad65b96398d06d9191
Nicolas <nicolaskrusser@gmail.com>
2018-09-24 18:49:05 +0200

    added som text

--- src/App.js
+++ src/App.js
@@ -2,8 +2,8 @@ import React, { Component } from 'react'
 import {Route, Switch} from 'react-router-dom';
 import Wrap from './hoc/Wrap/Wrap'
 import Layout from './hoc/Layout/Layout'
-import HomePage from './component/HomePage/HomePage';
-import AboutMe from './component/AboutMe/AboutMe';
+import HomePage from './component/Pages/HomePage/HomePage';
+import AboutMe from './component/Pages/AboutMe/AboutMe';
 
 class App extends Component {
   render() {
---
+++ src/component/Pages/HomePage/HomePage.js
@@ -0,0 +1,24 @@
+import React from 'react';
+import classes from './HomePage.css';
+
+const homePage = (props)=>(
+    <section className={classes.HomePage}>
+        <h1>I am Mykolaj Krusser</h1>
+        <article>
+            <p>
+                I am a creative front‑end developer.
+            </p>
+            <p>
+                I develop compelling designs that spring to life using transition and animations.
+            using the most sophisticated technologies available today for fully 
+            interactive and responsive websites and apps.
+            </p>
+            <p>
+                I’m passionate about the power of colours and shapes in developing a great new design, 
+            and JavaScript offers me endless fascinating possibilities in devising just the right solution.
+            </p>
+        </article>
+    </section>
+);
+
+export default homePage;
