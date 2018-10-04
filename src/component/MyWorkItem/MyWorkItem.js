afd6662a5fa93c05fcdd5d262f24f137173a0e67
Nicolas <nicolaskrusser@gmail.com>
2018-10-04 12:47:09 +0200

    create google fire-base for text and pictures, change some styles for MyWorkItem

--- src/component/MyWorkItem/MyWorkItem.js
+++ src/component/MyWorkItem/MyWorkItem.js
@@ -4,7 +4,7 @@ import classes from './MyWorkItem.css';
 const mywork = (props)=>(
         <div className={classes.Col} onTouchStart="this.classList.toggle('hover');">
             <div className={classes.Container}>
-                <div className={classes.Front} style={{backgroundImage: "url(https://unsplash.it/500/500/)"}}>
+                <div className={classes.Front} style={{backgroundImage: "url(https://firebasestorage.googleapis.com/v0/b/myportfolio-42a16.appspot.com/o/works_img%2F%D0%91%D0%B5%D0%B7%20%D0%BD%D0%B0%D0%B7%D0%B2%D0%B0%D0%BD%D0%B8%D1%8F.jpg?alt=media&token=80cc731d-2f65-4147-9a07-f1101aee54e2)"}}>
                     <div className={classes.Inner}>
                         <p>Diligord</p>
           <span>Lorem ipsum</span>
