e7cb5eddbbcdf5a4a70c0da98b1fb88ac153abc1
Nicolas <nicolaskrusser@gmail.com>
2018-10-06 19:28:53 +0200

    sent states to google firebase and got data from it

--- src/container/MyWorks/MyWorks.js
+++ src/container/MyWorks/MyWorks.js
@@ -6,52 +6,42 @@ import MyWorkItem from '../../component/MyWorkItem/MyWorkItem';
 class MyWorks extends Component{
     state = {
         works: [
+            /*
             { id: 1, title: 'Burger-builder', prevTitle: 'React app to create a burger.',
-                background: 'https://firebasestorage.googleapis.com/v0/b/myportfolio-42a16.appspot.com/o/works_img%2F698137.jpg?alt=media&token=3061d5c3-6ff5-440f-b210-1de291fd80f3',
-                git: 'https://github.com/MykolajKrusser',
-                live: 'https://google.com',
-                description:'an app to create a burger from individual ingredients, send an order to the server, and view a list of orders.',
-                category: ['react']
+            background: 'https://firebasestorage.googleapis.com/v0/b/myportfolio-42a16.appspot.com/o/works_img%2F698137.jpg?alt=media&token=3061d5c3-6ff5-440f-b210-1de291fd80f3',
+            git: 'https://github.com/MykolajKrusser',
+            live: 'https://google.com',
+            description:'an app to create a burger from individual ingredients, send an order to the server, and view a list of orders.',
+            category: ['react']
             },
             { id: 2, title: 'html page', prevTitle: 'Layout html, css, adaptive.',
-                background: 'https://firebasestorage.googleapis.com/v0/b/myportfolio-42a16.appspot.com/o/works_img%2Fbg.jpg?alt=media&token=71bb647b-a02c-4dd4-b850-427c98681cce',
-                git: 'https://github.com/MykolajKrusser',
-                live: 'https://google.com',
-                description:'Default layout use only css html.',
-                category: ['css']
-            },
-            { id: 3, title: 'html page', prevTitle: 'Layout html, css, adaptive.',
-                background: 'https://firebasestorage.googleapis.com/v0/b/myportfolio-42a16.appspot.com/o/works_img%2Fdevushki-stirka-dom-sobaka-bele-freier-evgenii.jpg?alt=media&token=8bd38c1a-d5b0-48a8-b6f7-31e9249eb13b',
-                links: 'https://github.com/MykolajKrusser',
-                description:'Default layout use only css html.',
-                category: ['css']
-            },
-            { id: 4, title: 'html page', prevTitle: 'Layout html, css, adaptive.',
-                links: 'www',
-                description:'Default layout use only css html.',
-                category: ['html']
-            },
-            { id: 5, title: 'html page', prevTitle: 'Layout html, css, adaptive.',
-                links: 'www',
-                description:'Default layout use only css html.',
-                category: ['react']
-            },
-            { id: 6, title: 'html page', prevTitle: 'Layout html, css, adaptive.',
-                links: 'www',
-                description:'Default layout use only css html.',
-                category: ['css']
+            background: 'https://firebasestorage.googleapis.com/v0/b/myportfolio-42a16.appspot.com/o/works_img%2Fbg.jpg?alt=media&token=71bb647b-a02c-4dd4-b850-427c98681cce',
+            git: 'https://github.com/MykolajKrusser',
+            live: 'https://google.com',
+            description:'Default layout use only css html.',
+            category: ['css']
             }
+            */
         ]
     }
 
+    send = ()=>{
+        const works = {
+           works: this.state.works
+        }
+        axios.post('/works.json', works )
+            .then(respons=>console.log(respons))
+            .catch(error=>console.log(error));
+    }
+    
     componentDidMount(){
-       axios.get('https://burger-app-4c52d.firebaseio.com/ingredients.json')
-         .then(respons=>{
-             this.setState({ingredients: respons.data})
-         })
-         .catch(error=>{
-             this.setState({error: true})
-         });
+        axios.get('https://myportfolio-42a16.firebaseio.com/works/-LO9ND2xIDnY-oPX0GDp/works.json')
+            .then(respons=>{
+                this.setState({works: respons.data})
+            })
+            .catch(error=>{
+                console.log(error)
+            });
     }
 
     render(){
