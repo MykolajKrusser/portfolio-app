33631bf4b52fc1ad5f8284a620dba2848eb0decb
Nicolas <nicolaskrusser@gmail.com>
2018-10-08 09:56:26 +0200

    added some tags and styles to the contact component

--- src/container/Contact/Contact.js
+++ src/container/Contact/Contact.js
@@ -8,7 +8,24 @@ class Contact extends Component{
     render(){
         return(
             <section className={classes.Contact}>
-                Contact
+                <form className={classes.Container}>
+                    <h2>Contact me</h2>
+                    <ul>
+                        <li>
+                            <label htmlFor="input">Your name</label>
+                            <input name="input" type="text" placeholder="input"/>
+                        </li>
+                        <li>
+                            <label htmlFor="input">Your e-mail</label>
+                            <input name="input" type="text" placeholder="input"/>
+                        </li>
+                        <li>
+                            <label htmlFor="textarea">Label</label>
+                            <textarea name="input" type="text" placeholder="text"/>
+                        </li>
+                    </ul>
+                    <input type="submit" name="submit" value="Send e-mail"/>
+                </form>
             </section>
         );
     }
