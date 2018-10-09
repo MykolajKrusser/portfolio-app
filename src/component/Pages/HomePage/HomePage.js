6f05bdffad77f8f233b666aefb39ef78591bdd3e
Nicolas <nicolaskrusser@gmail.com>
2018-10-09 10:42:59 +0200

    created new background for homePage component

--- src/component/Pages/HomePage/HomePage.js
+++ src/component/Pages/HomePage/HomePage.js
@@ -1,25 +1,28 @@
 import React from 'react';
 import classes from './HomePage.css';
+import BackgroundHome from '../../UI/Background/BackgroundHome';
 
 const homePage = (props)=>(
     <section className={classes.HomePage}>
-     <div className={classes.BlurredBox}></div>
-        <h1>I am Mykolaj Krusser</h1>
-        <article>
-            <p>
-                I am a creative front‑end developer.
-            </p>
-            <p>
-                I develop compelling designs that spring to life using transition and animations.
-            using the most sophisticated technologies available today for fully 
-            interactive and responsive websites and apps.
-            </p>
-            <p>
-                I’m passionate about the power of colours and shapes in developing a great new design, 
-            and JavaScript offers me endless fascinating possibilities in devising just the right solution.
-            </p>
-        </article>
-       
+        <BackgroundHome/>
+        <div className={classes.Content}>
+            <h1>I am Mykolaj Krusser</h1>
+            <article>
+                <p>
+                    I am a creative front‑end developer.
+                </p>
+                <p>
+                    I develop compelling designs that spring to life using transition and animations.
+                using the most sophisticated technologies available today for fully 
+                interactive and responsive websites and apps.
+                </p>
+                <p>
+                    I’m passionate about the power of colours and shapes in developing a great new design, 
+                and JavaScript offers me endless fascinating possibilities in devising just the right solution.
+                </p>
+            </article>
+        </div>
+        
     </section>
 );
 
---
+++ src/component/UI/Background/BackgroundHome.js
@@ -0,0 +1,22 @@
+import React from 'react';
+import classes from './BackgroundHome.css';
+const backgroundHome = (props)=>(
+    <div className={classes.Box}>
+        <div className={classes.Waves}></div>
+        <div className={classes.Waves}></div>
+        <div className={classes.Waves}></div>
+        <div className={classes.Waves}></div>
+        <div className={classes.Waves}></div>
+        <div className={classes.Waves}></div>
+        <div className={classes.Waves}></div>
+        <div className={classes.Waves}></div>
+        <div className={classes.Waves}></div>
+        <div className={classes.Waves}></div>
+        <div className={classes.Waves}></div>
+        <div className={classes.Waves}></div>
+        <div className={classes.Waves}></div>
+        <div className={classes.Waves}></div>
+    </div>
+);
+
+export default backgroundHome;
