776fbcc25ff7102bd2846a8732a9192e3e7fb1c1
Nicolas <nicolaskrusser@gmail.com>
2018-10-18 17:41:56 +0200

    working with pages text

--- src/component/Pages/AboutMe/AboutMe.js
+++ src/component/Pages/AboutMe/AboutMe.js
@@ -5,13 +5,15 @@ import Background from '../../UI/Background/Background';
 const aboutMe = (props)=>(
     <section className={classes.AboutMe}>
     <Background/>
-        <h1>About me</h1>
-        <article>
-            Lorem Ipsum is simply dummy text of the printing 
-            and typesetting industry. Lorem Ipsum has been the industry's
-            standard dummy text ever since the 1500s, when an unknown printer 
-            took a galley of type and scrambled it to make a type specimen book.
-        </article>
+        <div className={classes.Content}>
+            <h1>About me</h1>
+            <article>
+                Lorem Ipsum is simply dummy text of the printing 
+                and typesetting industry. Lorem Ipsum has been the industry's
+                standard dummy text ever since the 1500s, when an unknown printer 
+                took a galley of type and scrambled it to make a type specimen book.
+            </article>
+        </div>
     </section>
 );
 
