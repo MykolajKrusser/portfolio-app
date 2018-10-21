40941129821a0373c22973d7c31d2d0372eba9c6
Nicolas <nicolaskrusser@gmail.com>
2018-10-21 13:51:46 +0200

    worked with contacts form

--- src/container/Contact/Contact.js
+++ src/container/Contact/Contact.js
@@ -1,20 +1,24 @@
 import React, {Component} from 'react';
 import axios from '../../axios';
 import classes from './Contact.css';
+import Modal from '../../component/UI//Modal/Modal';
 
 class Contact extends Component{
     state = {
         nameData: '',
         emailData: '',
         textData: '',
-        formSubmitBtnDisabled: true
+        formSubmitBtnDisabled: true,
+        messageCondition: false,
+        messageConditionSuccess: 'Your message sent!',
+        error: null
     }
 
-    ChangeFormName = (event) => {
+    changeFormName = (event) => {
         this.setState({nameData: event.target.value});
     }
 
-    ChangeFormMail = (event) => {
+    changeFormMail = (event) => {
         this.setState({emailData: event.target.value});
         const reg = /^\w+([.-]?\w+)*@\w+([.-]?\w+)*(\.\w{1,3})+$/;
         if (reg.test(this.state.emailData) === true) {
@@ -25,10 +29,11 @@ class Contact extends Component{
         }
     }
 
-    ChangeFormText = (event) => {
+    changeFormText = (event) => {
         this.setState({textData: event.target.value});
     }
-    orderHandler = (event)=>{
+
+    formDataHandler = (event)=>{
         event.preventDefault();
         const formData = {
             name : this.state.nameData,
@@ -37,29 +42,49 @@ class Contact extends Component{
         };
         axios.post('/contact.json', formData)
             .then(response=>{
-                alert('Email sent')
+                this.setState({messageCondition: true})
             })
             .catch(
-                error => {alert('NOT Email sent')}
+                error => {
+                    this.setState({error: error})
+                }
             )
     }
+
+    modalConfirm = ()=>{
+        this.setState({
+            nameData: '',
+            emailData: '',
+            textData: '',
+            formSubmitBtnDisabled: true,
+            messageCondition: false,
+            error: null
+        })
+    }
+
     render(){
         return(
             <section className={classes.Contact}>
-                <form onSubmit={this.orderHandler}>
+                <Modal 
+                    show={this.state.messageCondition}
+                    modalClosed= {this.modalConfirm}>
+                        {this.state.nameData}, {this.state.messageConditionSuccess}
+                        {this.state.error ? this.state.error.message : null}
+                </Modal>
+                <form onSubmit={this.formDataHandler}>
                     <h2>Contact me</h2>
                     <ul>
                         <li>
                             <label htmlFor="input">Your name</label>
-                            <input name="input" type="text" placeholder="Your name" value={this.state.nameData} onChange={this.ChangeFormName}/>
+                            <input title="Enter Your name" name="input" type="text" placeholder="Your name" required value={this.state.nameData} onChange={this.changeFormName}/>
                         </li>
                         <li>
                             <label htmlFor="input">Your e-mail</label>
-                            <input name="input" type="text" placeholder="e-mail" value={this.state.emailData} onChange={this.ChangeFormMail}/>
+                            <input title="Enter Your e-mail" name="input" type="email" placeholder="e-mail" required value={this.state.emailData} onChange={this.changeFormMail}/>
                         </li>
                         <li>
                             <label htmlFor="textarea">Your message</label>
-                            <textarea name="input" type="text" placeholder="Your message" value={this.state.textData} onChange={this.ChangeFormText}/>
+                            <textarea title="Enter Your message" name="input" type="text" placeholder="Your message" required value={this.state.textData} onChange={this.changeFormText}/>
                         </li>
                     </ul>
                     <input type="submit" name="submit" value="Send e-mail" disabled={this.state.formSubmitBtnDisabled}/>
