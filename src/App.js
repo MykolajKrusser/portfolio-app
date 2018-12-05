0a827ec538632622a8888d86ebc227ea0f8c2e1b
Nicolas <nicolaskrusser@gmail.com>
2018-12-05 18:52:56 +0100

    changed styles

--- src/App.js
+++ src/App.js
@@ -2,21 +2,34 @@ import React, { Component } from 'react';
 import {Route, Switch} from 'react-router-dom';
 import Layout from './hoc/Layout/Layout';
 import HomePage from './component/Pages/HomePage/HomePage';
-import AboutMe from './component/Pages/AboutMe/AboutMe';
-import MyWorks from './container/MyWorks/MyWorks';
-import Contact from './container/Contact/Contact';
-import Page404 from './component/UI/404Page/Page404';
+import asyncComponent from './hoc/asyncComponent/asyncComponent';
+
+const asyncAboutMe = asyncComponent(()=>{
+  return import('./component/Pages/AboutMe/AboutMe');
+});
+
+const asyncMyWorks = asyncComponent(()=>{
+  return import('./container/MyWorks/MyWorks');
+});
+
+const asyncContact = asyncComponent(()=>{
+  return import('./container/Contact/Contact');
+});
+
+const asyncPage404 = asyncComponent(()=>{
+  return import('./component/UI/404Page/Page404');
+});
 
 class App extends Component {
   render() {
     return (
         <Layout>
           <Switch>
-            <Route path='/contact' component={Contact}/>
-            <Route path='/my-works' component={MyWorks}/>
-            <Route path='/about-me' component={AboutMe}/>
+            <Route path='/contact' component={asyncContact}/>
+            <Route path='/my-works' component={asyncMyWorks}/>
+            <Route path='/about-me' component={asyncAboutMe}/>
             <Route path='/' exact component={HomePage}/>
-            <Route component={Page404}/>
+            <Route component={asyncPage404}/>
           </Switch>
         </Layout>
     );
