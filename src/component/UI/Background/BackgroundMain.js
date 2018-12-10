ad0f0f2bc11eab8ace17abf08f1dab0adff0d83d
Nicolas <nicolaskrusser@gmail.com>
2018-12-10 10:26:00 +0100

    fixed styles

--- src/component/UI/Background/BackgroundMain.js
+++ src/component/UI/Background/BackgroundMain.js
@@ -2,16 +2,6 @@ import React from 'react';
 import classes from './BackgroundMain.css';
 const background2 = (props)=>(
         <div className={classes.Background}>
-            {/* <div className={classes.RedDot}>
-                <div className={classes.Reflect}></div>
-            </div>
-            <div className={classes.Rect}></div>
-            <div className={classes.Rect}></div>
-            <div className={classes.Rect}></div>
-            <div className={classes.Rect}></div>
-            <div className={classes.Rect}></div>
-            <div className={classes.Rect}></div>
-            <div className={classes.Rect}></div> */}
         </div>
 );
 
